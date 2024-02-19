95c7dd7c6e3e41170f484f8dd1f05416cc0fd639
Mahesh R N <127485614+Mahesh-niveus@users.noreply.github.com>
2024-02-20 02:14:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "google" {
 }
 
 resource "google_compute_instance" "default" {
-  name         = "test-vm"
+  name         = "test-vm-3"
   machine_type = "e2-micro"
   
   zone         = "asia-south1-c"
